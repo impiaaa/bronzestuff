5f1931f3f417ad8ad39d06ced1fa4918240d8b59
Spencer Alves <impiaaa@gmail.com>
2014-03-31 21:49:17 -0400

    Loot!

--- src/main/java/net/boatcake/bronze/BronzeStuffMod.java
+++ src/main/java/net/boatcake/bronze/BronzeStuffMod.java
@@ -6,8 +6,11 @@ import net.minecraft.item.EnumToolMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemHoe;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
 import net.minecraft.item.ItemTool;
+import net.minecraft.util.WeightedRandomChestContent;
+import net.minecraftforge.common.ChestGenHooks;
 import net.minecraftforge.common.Configuration;
 import net.minecraftforge.common.EnumHelper;
 import net.minecraftforge.event.ForgeSubscribe;
@@ -21,53 +24,107 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-@Mod(modid=BronzeStuffMod.modID, name="Bronze stuff")
+@Mod(modid = BronzeStuffMod.modID, name = "Bronze stuff")
 public class BronzeStuffMod {
 	public static final String modID = "bronzestuff";
+
 	@Instance
 	public static BronzeStuffMod instance;
+
+	@SidedProxy(clientSide = "net.boatcake.bronze.ClientProxy", serverSide = "net.boatcake.bronze.Proxy")
+	public static Proxy proxy;
+
 	private ItemArmor bronzeHelmet;
 	private ItemArmor bronzeChestplate;
 	private ItemArmor bronzeLeggings;
 	private ItemArmor bronzeBoots;
-	@SidedProxy(clientSide="net.boatcake.bronze.ClientProxy", serverSide="net.boatcake.bronze.Proxy")
-	public static Proxy proxy;
 	private ItemTool bronzePickaxe;
 	private ItemTool bronzeAxe;
 	private ItemSword bronzeSword;
 	private ItemTool bronzeShovel;
 	private ItemHoe bronzeHoe;
-	public static Item bronzeHorseArmor;
+	private Item bronzeHorseArmor;
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent e) {
-		Configuration config = new Configuration(e.getSuggestedConfigurationFile());
-	    EnumHelper.addToolMaterial("Bronze", 2, 350, 6.0F, 2.0F, 13);
-	    EnumArmorMaterial bronzeArmorMaterial = EnumHelper.addArmorMaterial("Bronze", 15, new int[] { 2, 6, 5, 2 }, 9);
-	    bronzePickaxe = new BronzePickaxe(4637, config, "bronze_pickaxe", EnumToolMaterial.IRON, 5.0F, "ingotBronze");
-	    bronzeAxe = new BronzeAxe(4638, config, "bronze_axe", EnumToolMaterial.IRON, 5.0F, "ingotBronze");
-	    bronzeSword = new BronzeSword(4639, config, "bronze_sword", EnumToolMaterial.IRON, 7, "ingotBronze");
-	    bronzeShovel = new BronzeShovel(4640, config, "bronze_shovel", EnumToolMaterial.IRON, 5.0F, "ingotBronze");
-	    bronzeHoe = new BronzeHoe(4641, config, "bronze_hoe", EnumToolMaterial.IRON, "ingotBronze");
-	    bronzeHelmet = new BronzeArmor(4642, config, "bronze_helmet", bronzeArmorMaterial, 0, "ingotBronze");
-	    bronzeChestplate = new BronzeArmor(4643, config, "bronze_chestplate", bronzeArmorMaterial, 1, "ingotBronze");
-	    bronzeLeggings = new BronzeArmor(4644, config, "bronze_leggings", bronzeArmorMaterial, 2, "ingotBronze");
-	    bronzeBoots = new BronzeArmor(4645, config, "bronze_boots", bronzeArmorMaterial, 3, "ingotBronze");
-	    //bronzeHorseArmor = new Item(config.getItem("IDs", "bronze_horse_armor", 4646).getInt(4646)).setUnlocalizedName("bronze_horse_armor").setMaxStackSize(1).setCreativeTab(CreativeTabs.tabMisc).setTextureName("bronze_horse_armor");
+		Configuration config = new Configuration(
+				e.getSuggestedConfigurationFile());
+		EnumHelper.addToolMaterial("Bronze", 2, 350, 6.0F, 2.0F, 13);
+		EnumArmorMaterial bronzeArmorMaterial = EnumHelper.addArmorMaterial(
+				"Bronze", 15, new int[] { 2, 6, 5, 2 }, 9);
+		bronzePickaxe = new BronzePickaxe(4637, config, "bronze_pickaxe",
+				EnumToolMaterial.IRON, 5.0F, "ingotBronze");
+		bronzeAxe = new BronzeAxe(4638, config, "bronze_axe",
+				EnumToolMaterial.IRON, 5.0F, "ingotBronze");
+		bronzeSword = new BronzeSword(4639, config, "bronze_sword",
+				EnumToolMaterial.IRON, 7, "ingotBronze");
+		bronzeShovel = new BronzeShovel(4640, config, "bronze_shovel",
+				EnumToolMaterial.IRON, 5.0F, "ingotBronze");
+		bronzeHoe = new BronzeHoe(4641, config, "bronze_hoe",
+				EnumToolMaterial.IRON, "ingotBronze");
+		bronzeHelmet = new BronzeArmor(4642, config, "bronze_helmet",
+				bronzeArmorMaterial, 0, "ingotBronze");
+		bronzeChestplate = new BronzeArmor(4643, config, "bronze_chestplate",
+				bronzeArmorMaterial, 1, "ingotBronze");
+		bronzeLeggings = new BronzeArmor(4644, config, "bronze_leggings",
+				bronzeArmorMaterial, 2, "ingotBronze");
+		bronzeBoots = new BronzeArmor(4645, config, "bronze_boots",
+				bronzeArmorMaterial, 3, "ingotBronze");
+//		bronzeHorseArmor = new Item(config.getItem("IDs", "bronze_horse_armor",
+//				4646).getInt(4646)).setUnlocalizedName("bronze_horse_armor")
+//				.setMaxStackSize(1).setCreativeTab(CreativeTabs.tabMisc)
+//				.setTextureName("bronze_horse_armor");
 	}
-	
+
 	@EventHandler
 	public void init(FMLInitializationEvent e) {
-		GameRegistry.addRecipe(new ShapedOreRecipe(bronzePickaxe, "III", " S ", " S ", 'I', "ingotBronze", 'S', "stickWood"));
-		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeAxe, "II", "IS", " S", 'I', "ingotBronze", 'S', "stickWood"));
-		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeAxe, "II", "SI", "S ", 'I', "ingotBronze", 'S', "stickWood"));
-		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeSword, "I", "I", "S", 'I', "ingotBronze", 'S', "stickWood"));
-		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeShovel, "I", "S", "S", 'I', "ingotBronze", 'S', "stickWood"));
-		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeHoe, "II", " S", " S", 'I', "ingotBronze", 'S', "stickWood"));
-		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeHoe, "II", "S ", "S ", 'I', "ingotBronze", 'S', "stickWood"));
-		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeHelmet, "III", "I I", 'I', "ingotBronze"));
-		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeChestplate, "I I", "III", "III", 'I', "ingotBronze"));
-		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeLeggings, "III", "I I", "I I", 'I', "ingotBronze"));
-		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeBoots, "I I", "I I", 'I', "ingotBronze"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(bronzePickaxe, "III", " S ",
+				" S ", 'I', "ingotBronze", 'S', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeAxe, "II", "IS", " S",
+				'I', "ingotBronze", 'S', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeAxe, "II", "SI", "S ",
+				'I', "ingotBronze", 'S', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeSword, "I", "I", "S",
+				'I', "ingotBronze", 'S', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeShovel, "I", "S", "S",
+				'I', "ingotBronze", 'S', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeHoe, "II", " S", " S",
+				'I', "ingotBronze", 'S', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeHoe, "II", "S ", "S ",
+				'I', "ingotBronze", 'S', "stickWood"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeHelmet, "III", "I I",
+				'I', "ingotBronze"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeChestplate, "I I",
+				"III", "III", 'I', "ingotBronze"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeLeggings, "III",
+				"I I", "I I", 'I', "ingotBronze"));
+		GameRegistry.addRecipe(new ShapedOreRecipe(bronzeBoots, "I I", "I I",
+				'I', "ingotBronze"));
+
+		ItemStack helmet = new ItemStack(bronzeHelmet);
+		ItemStack chestplate = new ItemStack(bronzeChestplate);
+		ItemStack leggings = new ItemStack(bronzeLeggings);
+		ItemStack boots = new ItemStack(bronzeBoots);
+		ItemStack pickaxe = new ItemStack(bronzePickaxe);
+		ItemStack sword = new ItemStack(bronzeSword);
+		// ItemStack horseArmor = new ItemStack(bronzeHorseArmor);
+		ChestGenHooks.addItem(ChestGenHooks.MINESHAFT_CORRIDOR,
+				new WeightedRandomChestContent(pickaxe, 1, 1, 1));
+		ChestGenHooks.addItem(ChestGenHooks.STRONGHOLD_CROSSING,
+				new WeightedRandomChestContent(pickaxe, 1, 1, 1));
+		WeightedRandomChestContent[] bronzeToolsArmor = {
+				new WeightedRandomChestContent(pickaxe, 1, 1, 3),
+				new WeightedRandomChestContent(sword, 1, 1, 3),
+				new WeightedRandomChestContent(helmet, 1, 1, 3),
+				new WeightedRandomChestContent(chestplate, 1, 1, 3),
+				new WeightedRandomChestContent(leggings, 1, 1, 3),
+				new WeightedRandomChestContent(boots, 1, 1, 3) };
+		for (WeightedRandomChestContent item : bronzeToolsArmor) {
+			ChestGenHooks.addItem(ChestGenHooks.STRONGHOLD_CROSSING, item);
+			ChestGenHooks.addItem(ChestGenHooks.PYRAMID_DESERT_CHEST, item);
+			ChestGenHooks.addItem(ChestGenHooks.PYRAMID_JUNGLE_CHEST, item);
+			ChestGenHooks.addItem(ChestGenHooks.STRONGHOLD_CORRIDOR, item);
+			ChestGenHooks.addItem(ChestGenHooks.VILLAGE_BLACKSMITH, item);
+		}
 	}
 }
